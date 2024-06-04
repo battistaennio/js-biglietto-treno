1cc6445166ececb38f4be56040e942468116816a
battistaennio <ennio.battista99@gmail.com>
2024-06-04 14:55:04 +0200

    Creazione diagramma di flusso

--- mainjavascript.js
+++ mainjavascript.js
@@ -3,3 +3,28 @@ Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio,
 il prezzo del biglietto è definito in base ai km (0.21 € al km)
 va applicato uno sconto del 20% per i minorenni
 va applicato uno sconto del 40% per gli over 65.*/
+
+
+
+//chiedo età
+
+//chiedo numero chilometri da percorrere
+
+//creo valore km * 0.21 standard
+
+//creo valore per età utente minorenne (numeri <= 17)
+
+//creo valore per età utente over 65 (numeri >= 65)
+
+//creo valore per età utente normale (numeri fra 18 e 64) 
+
+//se età è utente normale moltiplico per valore km standard
+
+	//altrimenti età minorenne moltiplico per valore standard e tolgo il 20%
+
+	//altimenti età over 65 moltiplico per valore standard e tolgo il 40%
+
+
+//approssimo il risultato alla seconda cifra decimale se necessario
+
+//comunico prezzo finale
